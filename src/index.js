9081cbeaf28a6b9947a045fa9db12ff0f87d4c85
Alex Ware <alex.ware@guardian.co.uk>
2017-05-11 12:35:40 +0100

    Prepare for multi lambda support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import AWS from 'aws-sdk';
 import {loadFromS3} from './config'
+import repeat from 'lodash/repeat'
 
 export function handler(event, context, callback) {
   console.log("Initialising.")
   let creds = loadFromS3()
   creds.then((creds)=>{
     console.log(creds)
+    console.log(repeat('hello',10))
     callback(null)
   }).catch((error)=>{
     console.error("Failed to get credentials",error)
