35d1fc5b0c19a5b46167ee3a46fa83665f66dbcf
Paul Roberts <paul.roberts@guardian.co.uk>
2019-07-31 14:06:21 +0100

    simplifly build-tc.sh

--- build-tc.sh
+++ build-tc.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-#Installing node 6.10
-NODE_VERSION="6.10"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install ${NODE_VERSION}
-nvm use ${NODE_VERSION}
+nvm install
+nvm use
 
-# Installing yarn
-YARN_VERSION="0.23.4"
-YARN_LOCATION="tools/${YARN_VERSION}"
-
-if [ ! -d "$YARN_LOCATION" ]; then
-	mkdir -p ${YARN_LOCATION}
-	cd ${YARN_LOCATION}/
-	wget -qO- https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz | tar zvx
-	cd ../..
-fi
-PATH="$PATH:${YARN_LOCATION}/dist/bin/"
+npm install -g yarn
 
 # Installing packages via yarn
 
@@ -32,6 +20,3 @@ yarn compile
 
 echo "BUNDLING AND UPLOADING TO RIFFRAFF"
 yarn riffraff
-
-
-
